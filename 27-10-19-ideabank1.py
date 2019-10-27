d1288c62eed64ec2949a2c4a0fe985481ef00226
Grzegorz Kalinowski <C:\Users\grzeg\AppData\Roaming\The Bat!>
2019-10-27 18:56:36 +0100

    commit

--- 27-10-19-ideabank1.py
+++ 27-10-19-ideabank1.py
@@ -1,1 +1,29 @@
-# idea bank
+# idea bank v1
+
+def write_to_file(typed_idea):          #zapisujemy idea
+    file = open("ideas.txt","a") 
+    file.write(typed_idea)
+    file.write("\n")
+    file.close()
+    
+def read_file():                        #czytamy plik
+    file = open("ideas.txt", "r")
+    x = 0
+    for line in file:
+        x = x + 1
+        print(str(x)+"." , line)
+    file.close()
+    
+def main():
+    print("What is your new idea:")
+    typed_idea = input()
+    if typed_idea == "delete":              #funkcja do kasowania pliku
+        file = open("ideas.txt", "w")
+        file.close()
+        print("File Deleted")
+        exit
+    else:
+        write_to_file(typed_idea)
+        read_file()
+
+main()
